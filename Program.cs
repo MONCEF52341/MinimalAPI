9df936e7a25fd60cb8c031e64998bbd725ea251d
MONCEF52341 <mostaine.moncef177@gmail.com>
2025-11-30 10:45:28 +0100

    Add Endpoint

--- Program.cs
+++ Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/test", (string version) => $"DEMO Hello World! Version = {version}");
 
 app.Run();
